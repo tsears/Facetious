d9ad554180300957a97a0504c143b85d84b09168
Tom Sears <tom@tsears.net>
2017-04-18 21:09:56 -0700

    Add duck reactor

---
+++ app/reactors/DuckReactor.js
@@ -0,0 +1,23 @@
+module.exports = class DuckReactor {
+  constructor(client, state) {
+    this._reaction = 'duck';
+    this._triggers = ['・゜゜・。。・゜'];
+    this._state = state;
+    this._client = client;
+  }
+
+  get reaction() { return this._reaction; }
+
+  get triggers() { return this._triggers; }
+
+  react(channel, from) {
+    const coinFlip = Math.floor(Math.random() * 2);
+
+    if(coinFlip === 0) {
+        this._client.action(channel, 'alertly points');
+    } else {
+      this._client.say(channel, 'bark! bark!');
+    }
+
+  }
+}
